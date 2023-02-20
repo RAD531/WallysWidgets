9a016310b1feeab8b1b934e6e9fbaffe66408827
Geraint Palmer <geraint.palmer@kaweb.co.uk>
2023-02-20 10:43:43 +0000

    add db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\PackSizes;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        $now = Carbon::now();
+
+        PackSizes::insert([
+            ['packSize' => 250, 'created_at' => $now, 'updated_at' => $now],
+            ['packSize' => 500, 'created_at' => $now, 'updated_at' => $now],
+            ['packSize' => 1000, 'created_at' => $now, 'updated_at' => $now],
+            ['packSize' => 2000, 'created_at' => $now, 'updated_at' => $now],
+            ['packSize' => 5000, 'created_at' => $now, 'updated_at' => $now],
+        ]);
     }
 }
